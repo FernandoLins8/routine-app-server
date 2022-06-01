a92f71aba5b5b2dd341577ea22ff6e51cd34920d
Fernando Lins <fernandolins088@gmail.com>
2022-05-31 23:30:59 -0300

    Add/configure express-async-errors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata"
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors';
 import cors from 'cors'
 import router from './routes'
+import AppError from "./errors/AppError";
 const app = express()
 
 app.use(cors())
@@ -9,3 +11,16 @@ app.use(express.json())
 app.use(router)
 
 app.listen(3333, () => console.log('HTTP server running!'))
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
